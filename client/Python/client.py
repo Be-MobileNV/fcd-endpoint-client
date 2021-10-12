48c3400dd5a65a9bf319ad7a6e0ecb4e99adcfd8
Glenn Delanghe <glenn.delanghe@be-mobile.com>
2021-10-12 12:05:29 +0200

    refactor python module

--- client/Python/client.py
+++ client/Python/client.py
@@ -1,16 +1,35 @@
 import time
 import base64
 import logging
-from client.Python.config import gpsPosition
-import config
+import argparse
+import json
 import websockets
 
-try:
-    import thread
-except ImportError:
-    import _thread as thread
-
 close_wait_time = 10
+
+class gpsPosition:
+    def __init__(self, vehicleID, vehicleType, timestamp, lon, lat, heading, hdop, speed):
+        self.vehicleID = vehicleID
+        self.vehicleType = vehicleType
+        self.timestamp = timestamp
+        self.lon = lon
+        self.lat = lat
+        self.heading = heading
+        self.hdop = hdop
+        self.speed = speed
+
+    def toJson(self):
+        gps_position = {"vehicleId": self.vehicleID,
+                        "vehicleType": self.vehicleType,
+                        "timestamp": self.timestamp,
+                        "lon": self.lon,
+                        "lat": self.lat,
+                        "heading": self.heading,
+                        "hdop": self.hdop,
+                        "speed": self.speed,
+                        }
+        return json.dumps(gps_position)
+
 class WebSocket:
     def __init__(self, args):
         self.logger = self.setLogger(self, args)
@@ -47,8 +66,25 @@ class WebSocket:
         self.logger.info("Closing the websocket by sending a close message")
         self.client.close()
 
+def loadConfig():
+    parser = argparse.ArgumentParser(description="FCD Endpoint Websocket Client")
+    parser.add_argument('--address', type=str, default='127.0.0.1',
+                        help='The address of the server to send the data to')
+    parser.add_argument('--port', type=str, default='443',
+                        help='If ingress is between the client and server, use 443, '
+                            'otherwise the same port as the server')
+    parser.add_argument('--tls', type=str, default="true",
+                        help='Usage of WSS(true) or WS(false).')
+    parser.add_argument('--username', type=str, default='',
+                        help='The username if you want to use basic authorization (securews must be set on true)')
+    parser.add_argument('--password', type=str, default='',
+                        help='The password if you want to use basic authorization (securews must be set on true)')
+
+    parser.add_argument('--verbose', default=False, action="store_true", help="Increase log verbosity", )
+    return parser.parse_args()
+
 def initiateWebSocket():
-    args = config.loadConfig()
+    args = loadConfig()
     ws = WebSocket(args)
     return ws
 
