b1b5790ca767dc643f97590a66ff45d310da0288
Jeroen Demeyer <jeroen.demeyer@be-mobile.com>
2021-10-14 11:30:25 +0200

    Fix Python client

--- client/Python/client.py
+++ client/Python/client.py
@@ -1,11 +1,14 @@
-import time, base64, logging, argparse, json, websockets, asyncio
+import base64
+import logging
+import json
+import websockets
+import asyncio
 
-close_wait_time = 10
 
-class gpsPosition:
-    def __init__(self, vehicleID, vehicleType, timestamp, lon, lat, heading, hdop, speed):
-        self.vehicleID = vehicleID
-        self.vehicleType = vehicleType
+class GPSPosition:
+    def __init__(self, vehicle_id, vehicle_type, timestamp, lon, lat, heading, hdop, speed):
+        self.vehicleId = vehicle_id
+        self.vehicleType = vehicle_type
         self.timestamp = timestamp
         self.lon = lon
         self.lat = lat
@@ -13,8 +16,8 @@ class gpsPosition:
         self.hdop = hdop
         self.speed = speed
 
-    def toJson(self):
-        gps_position = {"vehicleId": self.vehicleID,
+    def toJSON(self):
+        gps_position = {"vehicleId": self.vehicleId,
                         "vehicleType": self.vehicleType,
                         "timestamp": self.timestamp,
                         "lon": self.lon,
@@ -25,15 +28,26 @@ class gpsPosition:
                         }
         return json.dumps(gps_position)
 
+
 class WebSocket:
     def __init__(self, args):
-        self.logger = self.setLogger(args)
-        self.url = getURL(args)
+        self.logger = self._logger(args)
+        self.url = self._url(args)
         self.username = args.username
         self.password = args.password
-        self.positions = []
+
+    @staticmethod
+    def _url(args):
+        if args.tls in ["true", "True"]:
+            # Secured WSS protocol
+            url = "wss://{}:{}/v1/ws".format(args.address, args.port)
+        else:
+            # Unsecured WS protocol
+            url = "ws://{}:{}/v1/ws".format(args.address, args.port)
+        return url
     
-    def setLogger(self, args):
+    @staticmethod
+    def _logger(args):
         logger = logging.getLogger("FCD Endpoint Websocket Client")
         if args.verbose is True:
             logging.basicConfig(level=logging.DEBUG)
@@ -43,62 +57,32 @@ class WebSocket:
         logger.info("Loaded config")
         return logger
 
-    async def connect(self):
+    async def send(self, generator):
         header = []
         if self.username != "" and self.password != "":
             basic_auth_str = self.username + ":" + self.password
-            header = ["Authorization: Basic {}".format(base64.b64encode(basic_auth_str.encode('ascii')).decode())]
+            header = [("Authorization", "Basic {}".format(base64.b64encode(basic_auth_str.encode('ascii')).decode()))]
         self.logger.info("Connecting to {}".format(self.url))
-        async for websocket in  websockets.connect(self.url, extra_headers=header, ping_interval=60, ping_timeout=10, close_timeout=10):
-            try:
-                async for pos in self.GPSPositions():
-                    self.logger.info("Sending GPS position")
-                    websocket.send(pos)
-            except websockets.ConnectionClosed:
-                continue
-    
-    async def GPSPositions(self):
-        for pos in self.positions:
-            yield pos
-            self.positions.remove(pos)
+        async for websocket in websockets.connect(self.url, extra_headers=header, ping_interval=60, ping_timeout=10, close_timeout=10):
+            task = asyncio.create_task(self.handle_replies(websocket))
+            async for pos in generator:
+                self.logger.info("Sending GPS position")
+                # We need to wrap websocket.send(pos) in a Task in order to handle exceptions
+                sendtask = asyncio.create_task(websocket.send(pos))
+                try:
+                    await sendtask
+                except Exception as E:
+                    self.logger.error(E)
+                    break  # Force a reconnection
+            else:
+                # generator finished, exit
+                break
+            task.cancel()
 
-    def sendGPSPosition(self, position):
-        self.logger.debug("GPS position JSON: {}".format(position))
-        self.positions.append(position)
-        
-
-    def close(self):
-        self.logger.info("Closing the websocket by sending a close message")
-        self.client.close()
-
-def initiateWebSocket():
-    args = loadConfig()
-    ws = WebSocket(args)
-    asyncio.run(ws.connect())
-    return ws
-
-def loadConfig():
-    parser = argparse.ArgumentParser(description="FCD Endpoint Websocket Client")
-    parser.add_argument('--address', type=str, default='127.0.0.1',
-                        help='The address of the server to send the data to')
-    parser.add_argument('--port', type=str, default='443',
-                        help='If ingress is between the client and server, use 443, '
-                            'otherwise the same port as the server')
-    parser.add_argument('--tls', type=str, default="true",
-                        help='Usage of WSS(true) or WS(false).')
-    parser.add_argument('--username', type=str, default='',
-                        help='The username if you want to use basic authorization (securews must be set on true)')
-    parser.add_argument('--password', type=str, default='',
-                        help='The password if you want to use basic authorization (securews must be set on true)')
-
-    parser.add_argument('--verbose', default=False, action="store_true", help="Increase log verbosity", )
-    return parser.parse_args()
-
-def getURL(args):
-    if args.tls in ["true", "True"]:
-        # Secured WSS protocol
-        url = "wss://{}:{}/v1/ws".format(args.address, args.port)
-    else:
-        # Unsecured WS protocol
-        url = "ws://{}:{}/v1/ws".format(args.address, args.port)
-    return url
+    async def handle_replies(self, websocket):
+        """
+        Handle replies from the FCD endpoint server by logging them. Normally,
+        there are no replies: the only replies are error messages.
+        """
+        async for reply in websocket:
+            self.logger.error(reply)
