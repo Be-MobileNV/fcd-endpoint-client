9b681f6277c53b92ba5e460e94b96e245380cda2
Kilian VdK <kilian.van.de.kerckhove@be-mobile.com>
2022-02-15 10:45:00 +0100

    used pointers

--- client/Golang/pkg/config/config.go
+++ client/Golang/pkg/config/config.go
@@ -19,27 +19,27 @@ type WebSocketConfiguration struct {
 }
 
 type GPSPosition struct {
-	VehicleId   string            `json:"vehicleId"`
-	VehicleType int32             `json:"vehicleType,omitempty"`
-	EngineState int32             `json:"engineState,omitempty"`
-	Timestamp   int64             `json:"timestamp"`
-	Lon         float64           `json:"lon"`
-	Lat         float64           `json:"lat"`
-	Heading     float32           `json:"heading,omitempty"`
-	Hdop        float32           `json:"hdop,omitempty"`
-	Speed       float32           `json:"speed,omitempty"`
-	Alt         float32           `json:"alt,omitempty"`
-	Metadata    map[string]string `json:"metadata,omitempty"`
+	VehicleId   string             `json:"vehicleId"`
+	VehicleType *int32             `json:"vehicleType,omitempty"`
+	EngineState *int32             `json:"engineState,omitempty"`
+	Timestamp   int64              `json:"timestamp"`
+	Lon         float64            `json:"lon"`
+	Lat         float64            `json:"lat"`
+	Heading     *float32           `json:"heading,omitempty"`
+	Hdop        *float32           `json:"hdop,omitempty"`
+	Speed       *float32           `json:"speed,omitempty"`
+	Alt         *float32           `json:"alt,omitempty"`
+	Metadata    *map[string]string `json:"metadata,omitempty"`
 }
 
 func (g *GPSPosition) Validate() bool {
 	if unsafe.Sizeof(g.VehicleId) > 64 {
 		return false
 	}
-	if g.VehicleType < 0 || g.VehicleType > 19 {
+	if *g.VehicleType < 0 || *g.VehicleType > 19 {
 		return false
 	}
-	if g.EngineState < -1 || g.EngineState > 1 {
+	if *g.EngineState < -1 || *g.EngineState > 1 {
 		return false
 	}
 	if g.Lat < -90 || g.Lat > 90 {
@@ -48,13 +48,13 @@ func (g *GPSPosition) Validate() bool {
 	if g.Lon < -180 || g.Lon > 180 {
 		return false
 	}
-	if g.Heading < 0 || g.Heading >= 360 {
+	if *g.Heading < 0 || *g.Heading >= 360 {
 		return false
 	}
-	if g.Hdop < 0 {
+	if *g.Hdop < 0 {
 		return false
 	}
-	if g.Speed < 0 {
+	if *g.Speed < 0 {
 		return false
 	}
 	return true
