76b368c8b6489ed48fd126871ead59f5db15fe49
Jelle Bailleul <jelle.bailleul@be-mobile.com>
2024-03-12 15:22:14 +0100

    CVP-1537 update PR

--- client/Golang/pkg/config/gps_position.go
+++ client/Golang/pkg/config/gps_position.go
@@ -2,6 +2,11 @@ package config
 
 import (
 	"fmt"
+	"time"
+)
+
+var (
+	year2000MUnixMilli = time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC).UnixMilli()
 )
 
 type GPSPosition struct {
@@ -12,9 +17,9 @@ type GPSPosition struct {
 	Lon         float64           `json:"lon"`
 	Lat         float64           `json:"lat"`
 	Heading     *float32          `json:"heading,omitempty"` // in degrees
-	Hdop        *float32          `json:"hdop,omitempty"`
-	Speed       *float32          `json:"speed,omitempty"` // in km/h
-	Alt         *float32          `json:"alt,omitempty"`
+	Hdop        *float32          `json:"hdop,omitempty"`    // in meter
+	Speed       *float32          `json:"speed,omitempty"`   // in km/h
+	Alt         *float32          `json:"alt,omitempty"`     // in meter
 	Metadata    map[string]string `json:"metadata,omitempty"`
 }
 
@@ -23,19 +28,25 @@ func (g *GPSPosition) Validate() error {
 		return fmt.Errorf("vehicleId length cannot be longer than 63, while given length is %d", len(g.VehicleId))
 	}
 	if g.VehicleType != nil && *g.VehicleType < 0 {
-		return fmt.Errorf("invalid non-nil vehicle type:  must be positive")
+		return fmt.Errorf("invalid non-nil vehicle type: must be positive")
 	}
 	if g.EngineState != nil && (*g.EngineState < -1 || *g.EngineState > 1) {
 		return fmt.Errorf("invalid non-nil engine state: must be in interval [-1, 1]")
 	}
-	if g.Lat < -90 || g.Lat > 90 {
+	if !(g.Lat >= -90 && g.Lat <= 90) {
 		return fmt.Errorf("invalid latitude: must be in interval [-90, 90]")
 	}
-	if g.Lon < -180 || g.Lon > 180 {
+	if !(g.Lon >= -180 && g.Lon <= 180) {
 		return fmt.Errorf("invalid longitude: must be in interval [-180, 180]")
 	}
-	if g.Heading != nil && (*g.Heading < 0 || *g.Heading >= 360) {
-		return fmt.Errorf("invalid non-nil heading: must be in interval [0, 360]")
+	if g.Lon == 0 && g.Lat == 0 {
+		return fmt.Errorf("coordinates (0,0) not allowed")
+	}
+	if g.Timestamp < year2000MUnixMilli {
+		return fmt.Errorf("invalid timestamp: must be after 1 janaruary 2000 00h00")
+	}
+	if g.Heading != nil && !(*g.Heading >= 0 && *g.Heading < 360) {
+		return fmt.Errorf("invalid non-nil heading: must be in interval [0, 360[")
 	}
 	if g.Hdop != nil && *g.Hdop < 0 {
 		return fmt.Errorf("invalid non-nil hdop: must be positive")
